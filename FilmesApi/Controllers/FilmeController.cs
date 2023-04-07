96ff2ee8430e07c0bdd1c4f44b4a38e3f47125ef
Bruno Severo <brscabral07@gmail.com>
2023-04-07 19:55:15 -0300

    Iniciando o projeto e terminando o metodo post, get e o get pelo id dos filmes

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,34 @@
+﻿using FilmesApi.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]  
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id=0;
+
+    [HttpPost]
+
+    public void adicionarFilmes([FromBody]Filme filme)
+    {
+        filme.id = id++;
+
+        filmes.Add(filme);
+    }
+
+    [HttpGet]
+    public List<Filme> retornaFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? retornaFilmesId(int id)
+    {
+        return filmes.FirstOrDefault(filmes => filmes.id == id);
+
+    }
+}
---
+++ FilmesApi/models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.models
+{
+    public class Filme
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "O parametro de titulo não pode ser vazio")]
+        public string titulo { get; set; }
+        [Required(ErrorMessage = "O parametro de genero não pode ser vazio")]
+        [MaxLength(50, ErrorMessage ="A duração da descrição não pode ultrapassar 50 caracteres")]
+        public string genero { get; set; }
+        [Required(ErrorMessage = "O parametro de duração não pode ser vazio")]
+        [Range(70,600, ErrorMessage = "O filme não poderá ter uma duração menor do que 70 minutos ou maior que 600 minutos")]
+        public Int32 duracao { get; set; }
+
+
+    }
+}
